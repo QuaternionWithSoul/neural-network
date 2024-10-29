86fa02cbac797e5dbfb5a1f0f55e7468e2ff1d50
QuaternionWithSoul <nailzagru@gmail.com>
2024-10-29 22:08:24 +0300

    New generation of the project, from mnist to its own library

---
+++ src/data.rs
@@ -0,0 +1,30 @@
+use std::path::Path;
+use std::fs::{File, create_dir_all};
+use serde::{Serialize, Deserialize};
+
+use std::io::{Write, Read, Result, Error, ErrorKind};
+
+use bincode;
+
+use flate2::{write::GzEncoder, read::GzDecoder, Compression};
+
+pub fn serialize<T: Serialize>(data: &T, path: &str, name: &str) -> Result<()> {
+    let dir_path = Path::new(path);
+    if !dir_path.exists() { create_dir_all(dir_path)?; }
+    let file = File::create(dir_path.join(name))?;
+    let mut encoder = GzEncoder::new(file, Compression::default());
+    encoder.write_all(&bincode::serialize(data).unwrap())?;
+    encoder.finish()?;
+
+    Ok(())
+}
+
+pub fn deserialize<T: for<'de> Deserialize<'de>>(path: &str, name: &str) -> Result<T> {
+    let file = File::open(Path::new(path).join(name))?;
+    let mut decoded_data = Vec::new();
+    GzDecoder::new(file).read_to_end(&mut decoded_data)?;
+    let data: T = bincode::deserialize(&decoded_data)
+        .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+
+    Ok(data)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,29 @@
-use mnist::*;
+mod data;
 
-struct DataSet {
-    learn_images: Vec<u8>,
-    learn_result: Vec<u8>,
-    test_images: Vec<u8>,
-    test_result: Vec<u8>,
+use data::deserialize;
+
+use std::io::Result;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Data {
+    inputs: Vec<u8>,
+    outputs: u8
 }
 
-fn load_date_set(path: &str) -> DataSet {
-    let Mnist {
-        trn_img,
-        trn_lbl,
-        tst_img,
-        tst_lbl,
-        ..
-    } = MnistBuilder::new()
-        .base_path(path)
-        .finalize();
+fn main() -> Result<()> {
+    let path: &str = "data/mnist";
+    let name: &str = "learn-0";
 
-    return DataSet {
-        learn_images: trn_img,
-        learn_result: trn_lbl,
-        test_images: tst_img,
-        test_result: tst_lbl,
-    };
-}
+    println!("Extracting data from a dataset...");
+    let data: Vec<Data> = deserialize::<Vec<Data>>(path, name)?;
+    println!("Done!");
+    println!("");
 
-fn get_nomber(images: Vec<u8>, result: Vec<u8>, index: usize) {
-    println!("Result: {}", result[index]);
-
-    let first_image = &images[index*784..(index + 1)*784];
-    
-    for row in 0..28 {
-        for col in 0..28 {
-            let pixel = first_image[row * 28 + col];
-            if pixel > 0 { print!("##"); }
-            else { print!("  "); }
-        }
-        print!("\n");
+    for index in 0..1 {
+        println!("Inputs: {:?}", data[index].inputs);
+        println!("Outputs: {}", data[index].outputs);
     }
-}
 
-fn main() {
-    let DataSet {
-        learn_images: images, 
-        learn_result: result, 
-        ..
-    } = load_date_set("data/mnist");
-
-    get_nomber(images, result, 0);
+    Ok(())
 }
